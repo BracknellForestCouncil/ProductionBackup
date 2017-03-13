b9993e724ae5595286fa052b975c1e2a30f4c20c
Emily Coward <emily@microserve.io>
2017-03-13 11:46:58 +0000

    Added npm scripts.

---
+++ docrooot/sites/all/themes/custom/bracknell_theme_base/.bs-config.js
@@ -0,0 +1,25 @@
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+
+var mydevurl = "http://localhost";
+
+if (sourceFile = require('./.bs-config.local.js'))
+    mydevurl = sourceFile.myDevUrl;
+
+module.exports = {
+    "proxy": {
+        "target": mydevurl
+    },
+    "files": "css/*.css, js/*.js, templates/*.html.twig, !node_modules/**/*.html"
+};
