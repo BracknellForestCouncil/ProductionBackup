5ab92ed92afb687270c2ed0f683c0e44f6101bd9
Emily Coward <emily@microserve.io>
2017-03-21 17:19:56 +0000

    Removed bootstrap scripts and replaced with custom script for the main menu

--- docroot/sites/all/themes/custom/bracknell_theme_base/js/scripts.js
+++ docroot/sites/all/themes/custom/bracknell_theme_base/js/scripts.js
@@ -3,6 +3,52 @@
 (function ($) {
   Drupal.behaviors.bracknellThemeBaseBehavior = {
     attach: function attach(context, settings) {
+      $('[data-js="main-menu"]').find('.collapse').hide().attr('aria-hidden', 'true');
+
+      // Create main menu button.
+      var mainMenuButton = $('<button>' +
+        '<span class="button-copy pull-left"></span>' +
+        '<span aria-hidden="true" class="icon-bars-wrap pull-left">' +
+          '<span class="icon-bar top"></span>' +
+          '<span class="icon-bar centre"></span>' +
+          '<span class="icon-bar bottom"></span>' +
+        '</span>' +
+        '</button>');
+      mainMenuButton.attr({
+        'class': 'main-menu-btn',
+        'aria-expanded': 'false',
+        'aria-controls': 'main-nav',
+        'data-toggle': 'closed',
+        'data-js': 'main-menu-button'
+      });
+      mainMenuButton.find('.button-copy').text(Drupal.t('Menu'));
+      $('[data-js="main-menu-trigger"]').append(mainMenuButton);
+
+      $('[data-js="main-menu-button"]').on('click', function () {
+        var button = $(this),
+            status = button.attr('aria-expanded'),
+            mainMenu = $('[data-js="main-menu"]');
+
+        if (status === 'false') {
+          mainMenu.show().attr({
+            'aria-hidden': 'false'
+          });
+          button.attr({
+            'aria-expanded': 'true',
+            'data-toggle': 'open'
+          }).addClass('expanded').removeClass('collapsed');
+        }
+        else {
+          mainMenu.hide().attr({
+            'aria-hidden': 'true'
+          });
+          button.attr({
+            'aria-expanded': 'false',
+            'data-toggle': 'closed'
+          }).addClass('collapsed').removeClass('expanded');
+        }
+      });
+
       // * Brand logo resize on Menu open/close *
       // Click the Menu button, add .menu-open class to Brand logo wrapper
       // $('#open-menu').click(function () {
