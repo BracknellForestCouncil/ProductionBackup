8e4f6b9b124df0426d8ef0cca3b8a3a85d590368
Emily Coward <emcoward@hotmail.com>
2017-03-30 12:56:26 +0100

    Adjusted script for accordion

--- docroot/sites/all/modules/custom/localgov/localgov_base_paragraphs/js/localgov_base_paragraphs.js
+++ docroot/sites/all/modules/custom/localgov/localgov_base_paragraphs/js/localgov_base_paragraphs.js
@@ -20,12 +20,12 @@
     attach: function (context, settings) {
       if ($('.accordion-pane').length > 1) {
         $('.accordion').accAccordion({
-          containerClass: 'js-acc-tabs',
-          controlActiveClass: 'js-acc-tabs-control-item-active',
-          tabPanelClass: 'js-acc-tabs-panel',
-          panelActiveClass: 'js-tabs-panel-active',
-          controlsTextClass: 'element-invisible',
-          tabControlsClass: 'js-acc-tabs-control'
+          panelClass: 'js-accordion-panel',
+          panelId: 'js-accordion-panel-',
+          panelControlClass: 'js-accordion-control',
+          panelControlActiveClass: 'js-accordion-control-active',
+          panelControlId: 'js-accordion-control-',
+          panelTitleClass: 'js-accordion-panel-title'
         });
       }
     }
