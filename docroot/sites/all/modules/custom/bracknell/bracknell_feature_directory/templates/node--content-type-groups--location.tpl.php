90642f5fb5ed47ec8ccc18db983924bc1e84a5c0
Emily Coward <emily@microserve.io>
2017-04-11 17:18:28 +0100

    Adjusted HTML for directory pages and fields for location - leisure site.

--- docroot/sites/all/modules/custom/bracknell/bracknell_feature_directory/templates/node--content-type-groups--location.tpl.php
+++ docroot/sites/all/modules/custom/bracknell/bracknell_feature_directory/templates/node--content-type-groups--location.tpl.php
@@ -82,28 +82,21 @@
 hide($content['comments']);
 hide($content['links']);
 hide($content['field_tags']);
+hide($content['field_related']);
+hide($content['field_directory_entry']);
+hide($content['field_accessibility']);
+hide($content['field_promotional_hero']);
+hide($content['field_image']);
+
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
-  <header>
-    <?php print render($title_prefix); ?>
-    <?php if (!$page && !empty($title)): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
     <?php print render($map); ?>
-    <?php if ($display_submitted): ?>
-    <span class="submitted">
-      <?php print $user_picture; ?>
-      <?php print $submitted; ?>
-    </span>
-    <?php endif; ?>
-  </header>
   <?php endif; ?>
   <?php
     print render($content);
   ?>
-    
+
   <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
   <footer>
     <?php print render($content['field_tags']); ?>
