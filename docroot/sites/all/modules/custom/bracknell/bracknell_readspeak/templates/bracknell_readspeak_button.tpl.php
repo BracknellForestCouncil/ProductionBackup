56979b185eff4b35bf5a90c8e4c4847a750facf2
Emily Coward <emily@microserve.io>
2017-04-28 10:59:56 +0100

    Refactored code in readspeaker module to use theme function and a template.

---
+++ docroot/sites/all/modules/custom/bracknell/bracknell_readspeak/templates/bracknell_readspeak_button.tpl.php
@@ -0,0 +1,12 @@
+<div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve">
+  <a class="rsbtn_play" accesskey="L" title="Listen to this page using ReadSpeaker" href="http://app.eu.readspeaker.com/cgi-bin/rsent?customerid=5766&amp;lang=en_uk&amp;readid=readspeaker_content&amp;url=<?php print $rs_path ?>">
+    <span class="rsbtn_left rsimg rspart">
+      <span class="rsbtn_text">
+        <span>
+          <?php print t('Listen') ?>
+        </span>
+      </span>
+    </span>
+    <span class="rsbtn_right rsimg rsplay rspart"></span>
+  </a>
+</div>
