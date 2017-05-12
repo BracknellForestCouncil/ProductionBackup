3266758aabc8e9bfba2dd8a0e860e4a3fbf4a6aa
Emily Coward <emily@microserve.io>
2017-05-12 11:40:03 +0100

    Fixed display issues with the services menu.

--- docroot/sites/all/modules/custom/localgov/localgov_base_navigation/templates/navigation-homepage.tpl.php
+++ docroot/sites/all/modules/custom/localgov/localgov_base_navigation/templates/navigation-homepage.tpl.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * Please note, the spacing and alignment of the <li>s for the services menu
+ * are intentionally on one line in order for 'display: inline-block' to work
+ * correctly and display the services menu as required. Please do not change this unless * you intend to refactor this section completely. This issue is due to the original
+ * developer's approach.
+ */
+?>
+
 <h1 class="services-title">Services</h1>
 
 <div id="homepage-nav-wrapper" class="services-content grey-x-light full-width">
@@ -9,14 +18,10 @@
         $li_classes[] = 'nav-link-collapse';
         $li_classes[] = 'collapse';
       }
-    ?>
-    <li class="<?php print implode($li_classes, ' '); ?>" data-griddercontent="#homepage-nav-<?php print $item['name_safe']; ?>">
-      <?php
+    ?><li class="<?php print implode($li_classes, ' '); ?>" data-griddercontent="#homepage-nav-<?php print $item['name_safe']; ?>"><?php
         $icon = '<span class="icon nav-icon-' . $item['name_safe'] . '"></span>';
         print l($item['name'] . $icon, $item['link_path'], array('html' => TRUE));
-      ?>
-    </li>
-    <?php endforeach; ?>
+      ?></li><?php endforeach; ?>
   </ul>
 </div>
 
@@ -31,12 +36,8 @@ foreach ($menu_tree as $index => $item):
 ?>
 <div id="homepage-nav-<?php print $item['name_safe']; ?>" class="full-width gridder-content nav-content">
   <ul>
-    <?php foreach ($item['children'] as $child_item): ?>
-      <li class="nav-link"><?php print l($child_item['name'], $child_item['link_path']); ?></li>
-    <?php endforeach; ?>
-    <li class="nav-link nav-link-primary">
-      <?php print l('All of <strong>' . $item['name'] . '</strong>', '/' . $item['name_safe'], array('html' => TRUE)); ?>
-    </li>
+    <?php foreach ($item['children'] as $child_item): ?><li class="nav-link"><?php print l($child_item['name'], $child_item['link_path']); ?></li><?php endforeach; ?><li class="nav-link nav-link-primary">
+      <?php print l('All of <strong>' . $item['name'] . '</strong>', '/' . $item['name_safe'], array('html' => TRUE)); ?></li>
   </ul>
 </div>
 <?php endforeach; ?>
