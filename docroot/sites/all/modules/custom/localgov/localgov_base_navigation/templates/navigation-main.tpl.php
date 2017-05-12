9cc098c8af0fdca84938a22c803b270132d6f8fa
Emily Coward <emily@microserve.io>
2017-05-12 11:02:24 +0100

    Changed URL for my account link and removed tooltip.

--- docroot/sites/all/modules/custom/localgov/localgov_base_navigation/templates/navigation-main.tpl.php
+++ docroot/sites/all/modules/custom/localgov/localgov_base_navigation/templates/navigation-main.tpl.php
@@ -1,7 +1,7 @@
 <nav id="main-nav" role="navigation">
   <ul>
     <li class="visible-sm-inline-block visible-xs-inline-block nav-tools__account nav-item nav-item-my-account">
-      <a href="https://bracknell-forest.achieveservice.com/module/home">
+      <a href="https://myaccount.bracknell-forest.gov.uk/" title="<?php print t('Register for a self service account to save yourself time, or login if you already have an account.'); ?>">
         <span class="icon theme-icon-user"></span>
         <span><?php print t('My Account'); ?></span>
       </a>
