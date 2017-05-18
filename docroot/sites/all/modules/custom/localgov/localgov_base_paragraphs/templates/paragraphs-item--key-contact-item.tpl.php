b9190707ac1b505eafc6196fb9701041dbe134af
Emily Coward <emily@microserve.io>
2017-05-18 11:50:18 +0100

    Added field templates for key contacts to remove extra mark-up

---
+++ docroot/sites/all/modules/custom/localgov/localgov_base_paragraphs/templates/paragraphs-item--key-contact-item.tpl.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for entities.
+ *
+ * Available variables:
+ * - $content: An array of comment items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $title: The (sanitized) entity label.
+ * - $url: Direct url of the current entity if specified.
+ * - $page: Flag for the full page state.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. By default the following classes are available, where
+ *   the parts enclosed by {} are replaced by the appropriate values:
+ *   - entity-{ENTITY_TYPE}
+ *   - {ENTITY_TYPE}-{BUNDLE}
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
+ */
+?>
+<?php hide($content); ?>
+
+<div class="key-contact-item"<?php print $attributes; ?>>
+  <article class="key-contact-item-inner <?php print $classes; ?>" role="article">
+    <div class="key-contact-item-image">
+      <?php print render($content['field_key_contact_image']); ?>
+    </div>
+    <div class="key-contact-item-content">
+      <h3 class="key-contact-item-title">
+        <?php print render($content['field_key_contact_item_title']); ?>
+      </h3>
+      <p class="key-contact-item-email">
+        <?php print render($content['field_key_contact_email']); ?>
+      </p>
+    </div>
+  </article>
+</div>
