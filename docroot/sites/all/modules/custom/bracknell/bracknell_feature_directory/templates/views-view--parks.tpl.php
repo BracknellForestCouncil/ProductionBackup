7d5465c4d5096f31a56f838e8742c95573d98a04
Emily Coward <emily@microserve.io>
2017-05-24 12:03:34 +0100

    Added ID for aria-controls binding on parks view

--- docroot/sites/all/modules/custom/bracknell/bracknell_feature_directory/templates/views-view--parks.tpl.php
+++ docroot/sites/all/modules/custom/bracknell/bracknell_feature_directory/templates/views-view--parks.tpl.php
@@ -56,7 +56,7 @@
       <div class="row">
         <div class="col-sm-12">
           <div class="showcase">
-            <div class="row">
+            <div class="row" id="parks-view">
               <?php print $rows; ?>
             </div>
             <div class="showcase-footer"></div>
