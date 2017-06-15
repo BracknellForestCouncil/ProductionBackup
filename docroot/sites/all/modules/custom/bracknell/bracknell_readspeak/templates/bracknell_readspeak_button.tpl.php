903814044b56ad2b04beabe586e6ce9457711348
Maffoo <naiveamoeba@gmail.com>
2017-06-15 13:43:32 +0100

    Changed readspeak URL

--- docroot/sites/all/modules/custom/bracknell/bracknell_readspeak/templates/bracknell_readspeak_button.tpl.php
+++ docroot/sites/all/modules/custom/bracknell/bracknell_readspeak/templates/bracknell_readspeak_button.tpl.php
@@ -1,5 +1,5 @@
 <div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve">
-  <a class="rsbtn_play" accesskey="L" title="Listen to this page using ReadSpeaker" href="http://app.eu.readspeaker.com/cgi-bin/rsent?customerid=5766&amp;lang=en_uk&amp;readid=readspeaker_content&amp;url=<?php print $rs_path ?>">
+  <a class="rsbtn_play" accesskey="L" title="Listen to this page using ReadSpeaker" href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=5766&amp;lang=en_uk&amp;readid=readspeaker_content&amp;url=<?php print $rs_path ?>">
     <span class="rsbtn_left rsimg rspart">
       <span class="rsbtn_text">
         <span>
