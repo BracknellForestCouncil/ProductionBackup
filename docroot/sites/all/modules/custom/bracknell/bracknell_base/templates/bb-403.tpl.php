fc79b253215eef5a32aecd00e2a9a00dedbd2b75
Sebastian Anthony <seb.anthony@microserve.io>
2017-08-18 17:12:14 +0100

    now using template for 403 and 404 pages

---
+++ docroot/sites/all/modules/custom/bracknell/bracknell_base/templates/bb-403.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Custom 403 page html.
+ */
+
+?>
+
+<p>You are not authorized to access this page.</p>
---
+++ docroot/sites/all/modules/custom/bracknell/bracknell_base/templates/bb-404.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Custom 404 page html.
+ */
+
+?>
+
+<p>Sorry, the page you were looking for was not found.<br/><br/>
+There are a few different ways to help you find the page you were looking for:<br/>
+<ul>
+  <li>check that you typed in the correct address</li>
+  <li>try using our<em class="placeholder"> </em><a href="/search">search</a></li>
+  <li>go to the <a href="/">home page</a> and browse the site</li>
+</ul>
+</p>
