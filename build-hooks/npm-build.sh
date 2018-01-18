89a0656f732983c8592fc70b7c55ff13b27d1395
Marc Flannery <marc@care-planner.co.uk>
2018-01-18 11:43:59 +0000

    typo

--- build-hooks/npm-build.sh
+++ build-hooks/npm-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 BUILDPATH="$( cd "$(dirname "$0")/.." ; pwd -P )"
-cd "${BUILDPATH}/docroot/sites/all/themes/custom/bracknell_theme_base/
+cd "${BUILDPATH}/docroot/sites/all/themes/custom/bracknell_theme_base/"
 npm install
 ./node_modules/.bin/bower install
 npm run build:css
